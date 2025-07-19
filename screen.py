bedcc74c501f0ad4fa73a4940ef656378434a48c
Nenad Noveljic <18366081+nenadnoveljic@users.noreply.github.com>
2025-07-19 16:26:41 +0200

    Include stocks with quartal losses

--- screen.py
+++ screen.py
@@ -4,6 +4,7 @@ import datetime
 from lib.downloader import DIR,COPIED_DOWNLOADS_DIR, get_portfolio_filename_with_symbols
 from lib.data import SYMBOL, PE_PB, get_merged_pd
 
+DB_DIR = "."
 
 merged_df = get_merged_pd(os.path.join(COPIED_DOWNLOADS_DIR, 'PE.csv'), os.path.join(COPIED_DOWNLOADS_DIR, 'PB.csv'))
 
@@ -24,8 +25,26 @@ for year in range(current_year - 10, current_year):
     if os.path.exists(dividend_file):
         filtered_df = filter_out_stocks_from_file(filtered_df, dividend_file)
         
+with_quartal_loss = pd.read_csv(os.path.join(DB_DIR, 'with_quartal_loss.csv'))
+# Merge with_quartal_loss with filtered_df on SYMBOL
+merged_with_loss = pd.merge(
+    filtered_df,
+    with_quartal_loss[[SYMBOL]],
+    on=SYMBOL,
+    how='left',
+    indicator=True
+)
 
-sorted_df = filtered_df.sort_values(by=PE_PB, ascending=True)
+# Set 'quartal_loss' column: True if present in with_quartal_loss, else False
+merged_with_loss['quartal_loss'] = merged_with_loss['_merge'] == 'both'
+# Drop the merge indicator column
+merged_with_loss = merged_with_loss.drop(columns=['_merge'])
+
+# Use merged_with_loss as the new filtered_df for further processing
+filtered_df = merged_with_loss
+
+
+sorted_df = filtered_df.sort_values(by=['quartal_loss', PE_PB], ascending=[True, True])
 print(sorted_df.head(25))
 print(len(sorted_df))
       
